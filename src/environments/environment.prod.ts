70048b9f043c293fed2eb9d5f178025df0a6eadb
unknown <pgcuascotac@utn.edu.ec>
2021-09-10 21:34:22 -0500

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
-  API_URI:'http://localhost:3000/api',
-  API_URI1:'http://localhost:3000/login'
+
+  API_URI:'http://143.198.101.56:3000/api',
+  API_URI1:'http://143.198.101.56:3000/login'
+
+  // API_URI:'http://localhost:3000/api',
+  // API_URI1:'http://localhost:3000/login'
 };
