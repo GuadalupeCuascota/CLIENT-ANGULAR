8b8f6f68bd7b4d03f1862f15eb1e01ed053e3095
unknown <pgcuascotac@utn.edu.ec>
2021-09-11 14:42:15 -0500

    enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  // API_URI:'http://143.198.101.56:3000/api',
-  // API_URI1:'http://143.198.101.56:3000/login'
+  API_URI:'http://143.198.101.56:3000/api',
+  API_URI1:'http://143.198.101.56:3000/login'
 
-  API_URI:'http://localhost:3000/api',
-  API_URI1:'http://localhost:3000/login'
+  // API_URI:'http://localhost:3000/api',
+  // API_URI1:'http://localhost:3000/login'
   
 };
 
