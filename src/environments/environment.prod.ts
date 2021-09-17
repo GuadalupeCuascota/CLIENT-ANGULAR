bc539ff9737e253c108ebf6eae6be8ab174ed19e
unknown <pgcuascotac@utn.edu.ec>
2021-09-17 14:46:47 -0500

    conexion1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   // API_URI:'http://143.198.101.56:3000/api',
   // API_URI1:'http://143.198.101.56:3000/login'
 
-  API_URI:'http://localhost:3000/api',
-  API_URI1:'http://localhost:3000/login'
+  API_URI:'https://server-app-stem-elreq.ondigitalocean.app/api',
+  API_URI1:'https://server-app-stem-elreq.ondigitalocean.app/login'
 };
