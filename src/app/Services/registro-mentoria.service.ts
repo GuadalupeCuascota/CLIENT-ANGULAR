41183b9413d7c56be354eaf3330fe09077cb150a
unknown <pgcuascotac@utn.edu.ec>
2021-09-19 15:08:19 -0500

    registro mentoria modificado

--- src/app/Services/registro-mentoria.service.ts
+++ src/app/Services/registro-mentoria.service.ts
@@ -1,45 +1,46 @@
 import { Injectable } from '@angular/core';
 import{HttpClient} from '@angular/common/http'
 import {Mentoria} from '../Models/mentoria'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistroMentoriaService {
 
-  API_URI='http://localhost:3000/api';
+  
   filtroUsuario: "";
   
 
   constructor(private http: HttpClient) { }
 
   getMentorias(){
-    return this.http.get(`${this.API_URI}/registro-mentorias`);
+    return this.http.get(`${environment.API_URI}/registro-mentorias`);
   
   }
   getAgendamientoMentorias(){
-    return this.http.get(`${this.API_URI}/solicitudMentoria`);
+    return this.http.get(`${environment.API_URI}/solicitudMentoria`);
   
   }
   
   getMentoria (id:String){
-    return this.http.get(`${this.API_URI}/registro-mentorias/${id}`);
+    return this.http.get(`${environment.API_URI}/registro-mentorias/${id}`);
   
   }
   saveMentoria (formData){
-    return this.http.post(`${this.API_URI}/registro-mentorias`,formData);
+    return this.http.post(`${environment.API_URI}/registro-mentorias`,formData);
   
   }
   deleteMentoria (id:String){
-    return this.http.delete(`${this.API_URI}/registro-mentorias/${id}`);
+    return this.http.delete(`${environment.API_URI}/registro-mentorias/${id}`);
   
   }
 updateMentoria (id:String, updateMentoria:Mentoria){
-    return this.http.put(`${this.API_URI}/registro-mentorias/${id}`,updateMentoria);
+    return this.http.put(`${environment.API_URI}/registro-mentorias/${id}`,updateMentoria);
   
   }
   updateAgenMentoria(id:number, updateM:Mentoria){
-    return this.http.put(`${this.API_URI}/solicitudMentoria/${id}`,updateM);
+    return this.http.put(`${environment.API_URI}/solicitudMentoria/${id}`,updateM);
   }
   
   
