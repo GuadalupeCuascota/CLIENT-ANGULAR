8c1eba4df4812d8c94b1edd299f453243f6cb58b
unknown <pgcuascotac@utn.edu.ec>
2021-11-10 12:04:47 -0500

    api nueva

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   // API_URI:'http://localhost:5000/api',
   // API_URI1:'http://localhost:5000/login'
 
-  API_URI:'https://server-app-stem-elreq.ondigitalocean.app/api',
-  API_URI1:'https://server-app-stem-elreq.ondigitalocean.app/login'
+  API_URI:'https://wstem-server.herokuapp.com/api',
+  API_URI1:'https://wstem-server.herokuapp.com/login'
   
 };
 
