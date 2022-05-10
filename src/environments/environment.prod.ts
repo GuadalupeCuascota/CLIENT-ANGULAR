841c0c14eeacf0a2d6b037c59fc8ec9bb85eee63
unknown <pgcuascotac@utn.edu.ec>
2022-05-10 00:17:34 -0500

    api_new

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,8 @@ export const environment = {
   // API_URI:'http://143.198.101.56:3000/api',
   // API_URI1:'http://143.198.101.56:3000/login'
 
-  API_URI:'https://wstem-server.herokuapp.com/api',
-  API_URI1:'https://wstem-server.herokuapp.com/login'
+  // API_URI:'https://wstem-server.herokuapp.com/api',
+  // API_URI1:'https://wstem-server.herokuapp.com/login'
+  API_URI:' https://server-app-stem.herokuapp.com/api',
+  API_URI1:' https://server-app-stem.herokuapp.com/login'
 };
