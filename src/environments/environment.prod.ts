87b015190735e90393765ae4df68933c77704024
unknown <pgcuascotac@utn.edu.ec>
2022-07-24 14:47:42 -0500

    ultimo commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
   production: true,
+  ////SERVIDOR LOCAL
 
-  API_URI:'http://143.198.101.56:3000/api',
-  API_URI1:'http://143.198.101.56:3000/login'
+  // API_URI:'http://143.198.101.56:3000/api',
+  // API_URI1:'http://143.198.101.56:3000/login'
 
-
-
-
-  // API_URI:' https://server-app-stem.herokuapp.com/api',
-  // API_URI1:' https://server-app-stem.herokuapp.com/login'
+////SERVIDOR REMOTO
+  API_URI:' https://server-app-stem.herokuapp.com/api',
+  API_URI1:' https://server-app-stem.herokuapp.com/login'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,17 @@
 
 export const environment = {
   production: true,
+  ////SERVIDOR LOCAL
+
   // API_URI:'http://localhost:5000/api',
   // API_URI1:'http://localhost:5000/login'
 
+////SERVIDOR REMOTO
   API_URI:'https://server-app-stem.herokuapp.com/api',
   API_URI1:'https://server-app-stem.herokuapp.com/login'
 
 };
+
 
 
 
